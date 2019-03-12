7495c9eb9fc4cebd30f786360b345260c5e615d2
yiyc-kor <yiyc.kor@gmail.com>
2019-03-12 16:21:29 +0900

    feat: Studied Dataset

--- golbin/test.py
+++ golbin/test.py
@@ -1,14 +1,16 @@
+# Word2Vec 모델을 간단하게 구현해봅니다.
 import tensorflow as tf
-from tensorflow.contrib import learn
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+# matplot 에서 한글을 표시하기 위한 설정
 # font_name = matplotlib.font_manager.FontProperties(
-#                 fname="C:/Windows/Fonts/D2Coding/D2Coding-Ver1.3.2-20180524-all.ttc"
+#                 fname="/Library/Fonts/NanumGothic.otf"  # 한글 폰트 위치를 넣어주세요
 #             ).get_name()
 # matplotlib.rc('font', family=font_name)
 
+# 단어 벡터를 분석해볼 임의의 문장들
 sentences = ["나 고양이 좋다",
              "나 강아지 좋다",
              "나 동물 좋다",
@@ -24,24 +26,33 @@ sentences = ["나 고양이 좋다",
              "고양이 강아지 싫다",
              "강아지 고양이 좋다"]
 
-sentences_word_max_length = max([len(s.split(" ")) for s in sentences])
-sentences_processor = learn.preprocessing.VocabularyProcessor(sentences_word_max_length)
-x = np.array(list(sentences_processor.fit_transform(sentences)))
-
+# 문장을 전부 합친 후 공백으로 단어들을 나누고 고유한 단어들로 리스트를 만듭니다.
 word_sequence = " ".join(sentences).split()
+word_list = " ".join(sentences).split()
+word_list = list(set(word_list))
+# 문자열로 분석하는 것 보다, 숫자로 분석하는 것이 훨씬 용이하므로
+# 리스트에서 문자들의 인덱스를 뽑아서 사용하기 위해,
+# 이를 표현하기 위한 연관 배열과, 단어 리스트에서 단어를 참조 할 수 있는 인덱스 배열을 만듭합니다.
+word_dict = {w: i for i, w in enumerate(word_list)}
 
-del sentences
-
+# 윈도우 사이즈를 1 로 하는 skip-gram 모델을 만듭니다.
+# 예) 나 게임 만화 애니 좋다
+#   -> ([나, 만화], 게임), ([게임, 애니], 만화), ([만화, 좋다], 애니)
+#   -> (게임, 나), (게임, 만화), (만화, 게임), (만화, 애니), (애니, 만화), (애니, 좋다)
 skip_grams = []
 
 for i in range(1, len(word_sequence) - 1):
-    target = sentences_processor.vocabulary_.get(word_sequence[i])
-    context = [sentences_processor.vocabulary_.get(word_sequence[i - 1])], [sentences_processor.vocabulary_.get(word_sequence[i + 1])]
+    # (context, target) : ([target index - 1, target index + 1], target)
+    # 스킵그램을 만든 후, 저장은 단어의 고유 번호(index)로 저장합니다
+    target = word_dict[word_sequence[i]]
+    context = [word_dict[word_sequence[i - 1]], word_dict[word_sequence[i + 1]]]
 
+    # (target, context[0]), (target, context[1])..
     for w in context:
         skip_grams.append([target, w])
 
 
+# skip-gram 데이터에서 무작위로 데이터를 뽑아 입력값과 출력값의 배치 데이터를 생성하는 함수
 def random_batch(data, size):
     random_inputs = []
     random_labels = []
@@ -70,7 +81,8 @@ embedding_size = 2
 # batch_size 보다 작아야 합니다.
 num_sampled = 15
 # 총 단어 갯수
-voc_size = len(sentences_processor.vocabulary_)
+voc_size = len(word_list)
+
 
 #########
 # 신경망 모델 구성
@@ -100,3 +112,39 @@ loss = tf.reduce_mean(
             tf.nn.nce_loss(nce_weights, nce_biases, labels, selected_embed, num_sampled, voc_size))
 
 train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+
+#########
+# 신경망 모델 학습
+######
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    for step in range(1, training_epoch + 1):
+        batch_inputs, batch_labels = random_batch(skip_grams, batch_size)
+
+        _, loss_val = sess.run([train_op, loss],
+                               feed_dict={inputs: batch_inputs,
+                                          labels: batch_labels})
+
+        if step % 10 == 0:
+            print("loss at step ", step, ": ", loss_val)
+
+    # matplot 으로 출력하여 시각적으로 확인해보기 위해
+    # 임베딩 벡터의 결과 값을 계산하여 저장합니다.
+    # with 구문 안에서는 sess.run 대신 간단히 eval() 함수를 사용할 수 있습니다.
+    trained_embeddings = embeddings.eval()
+
+
+#########
+# 임베딩된 Word2Vec 결과 확인
+# 결과는 해당 단어들이 얼마나 다른 단어와 인접해 있는지를 보여줍니다.
+######
+for i, label in enumerate(word_list):
+    x, y = trained_embeddings[i]
+    plt.scatter(x, y)
+    plt.annotate(label, xy=(x, y), xytext=(5, 2),
+                 textcoords='offset points', ha='right', va='bottom')
+
+plt.show()
